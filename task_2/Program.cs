cd02927cea084cef9c0706135ede1fdda2b836a8
sergey akimenko <saguarointexas@mail.ru>
2022-10-09 19:24:10 +0300

    Tasks

---
+++ task_2/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного
+числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine());
+int numberThree = 0;
+if (number < 100)
+{
+    Console.WriteLine($"Третьей цифры нет");
+    return;
+}
+if (number > 100)
+{
+    while (number > 1000)
+    {
+        number = number / 10;
+    }
+    numberThree = number % 10;
+}
+Console.WriteLine($"Третья цифра числа -> {numberThree}" );
+
+
+
+
+
---
+++ task_3/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/ 
+
+Console.Write("Введите номер дня недели ");
+int number = int.Parse(Console.ReadLine());
+
+switch (number)
+{
+    case 1: 
+    Console.WriteLine("Нет");
+    break;
+    case 2: 
+    Console.WriteLine("Нет");
+    break;
+    case 3: 
+    Console.WriteLine("Нет");
+    break;
+    case 4: 
+    Console.WriteLine("Нет");
+    break;
+    case 5: 
+    Console.WriteLine("Нет");
+    break;
+    case 6: 
+    Console.WriteLine("Да");
+    break;
+    case 7: 
+    Console.WriteLine("Да");
+    break;
+}
+
+
